68756c6011fe8e71a0cca831dde296aa7ec09765
Candice <candice.doise@gmail.com>
2022-10-04 16:38:52 +0200

    fin premiere journée

--- app.js
+++ app.js
@@ -1,25 +1,71 @@
 let valueTextArea = document.querySelector(".coding-frame");
 let result = document.querySelector(".result");
+valueTextArea.style.resize = "none";
 
 valueTextArea.addEventListener('input', function handleChange(event) {
-   result.innerHTML = event.target.value;
+   
    let nbCaracters = valueTextArea.value.length;
+   let caractersBottom = result.innerText;
+   let nbCaractersBottom = caractersBottom.length;
+    if(nbCaractersBottom<200){
+        result.innerHTML = event.target.value;
+    }
+   
+   
     let bar = document.querySelector(".progress-bar");
-    let width = (nbCaracters / 2) + "%";
+    let width = (nbCaractersBottom / 2) + "%";
+
     bar.style.width = width;
-        if (nbCaracters <= 100) {
+        if (nbCaractersBottom <= 100) {
             bar.style.background = "green";
         }
-        else if (nbCaracters <= 170) {
+        else if (nbCaractersBottom <= 170) {
             bar.style.background = "orange";
         }
-        else if (nbCaracters <= 200) {
+        else if (nbCaractersBottom <= 200) {
             bar.style.background = "red";
         }
+    
   });
 
-  let mousemove = document.querySelector("#mouse-move");
 
-  window.addEventListener("mousemove", (e) =>{
-    mousemove.style.left = e.pageX + "px";
+  //Animation with mouse
+  let movemouse = document.querySelector("#mouse-move");
+
+  valueTextArea.onmouseout = function (event) {
+    movemouse.style.display = 'none';
+  };
+  valueTextArea.onmouseover = function (event) {
+    movemouse.style.display = 'block';
+  };
+
+
+  //buttons
+  let strong = document.querySelector(".strong");
+
+  strong.addEventListener('click', function addTag(){
+    valueTextArea.value += '<strong>' + '</strong>';
   });
+
+  let italic = document.querySelector(".italic");
+
+  italic.addEventListener('click', function addTag2(){
+    valueTextArea.value += '<em>' + '</em>';
+  });
+
+  //Dark mode
+  let dark = document.querySelector(".dark");
+  let body = document.querySelector("body");
+  dark.addEventListener('click', function darkMode(){
+    body.style.background = "#171717";
+    body.style.color = "#fff";
+    result.style.background = "#424242";
+    valueTextArea.style.background = "#424242";
+    result.style.color = "#fff";
+    valueTextArea.style.color = "#fff";
+    dark.style.background = "#000";
+    dark.style.color = "#fff";
+    dark.style.border = "1px solid white";
+  });
+
+  
